691da1dae647401c35218c8f00894334a76c41e6
eldrianayera <eldrianayera710@gmail.com>
2024-09-02 15:49:55 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-let multiplier = 0;
+const input = document.getElementById("number");
+const output = document.getElementById("result");
+const button = document.getElementById("calculate");
 
-const solution = (number) => {
+const solution = () => {
+  const number = input.value;
+  let multiplier = 0;
   for (i = 0; i < number; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
       multiplier += i;
     }
   }
-  return multiplier;
+  output.innerText = "Output : " + multiplier;
 };
 
-console.log(solution(200));
+button.addEventListener("click", solution);
 
 //This code add every
 // multiplier of 3 and 5 before the inputed number
 // if a number is both a
 // multiplier of 3 and 5 just add it once
-``;
